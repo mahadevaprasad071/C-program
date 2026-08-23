957be122b8749799acf71b4532c77064335ba690
Mahadeva Prasad <mahadevaprasad2572007@gmail.com>
2026-08-23 19:12:24 +0530

    Add prime number checker in P9.c

---
+++ P9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, flag = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (n > 1 && flag == 0)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
