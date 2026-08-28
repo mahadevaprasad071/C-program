8ac8acf673a4b764daa511bdf797ca5c00a82b7e
Mahadeva Prasad <mahadevaprasad2572007@gmail.com>
2026-08-28 21:20:18 +0530

    Implement digit counting for even and odd numbers

---
+++ P20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, even = 0, odd = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0) {
+        digit = n % 10;
+
+        if (digit % 2 == 0)
+            even++;
+        else
+            odd++;
+
+        n = n / 10;
+    }
+
+    printf("Even digits = %d\n", even);
+    printf("Odd digits = %d", odd);
+
+    return 0;
+}
